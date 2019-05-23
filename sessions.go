aa5b89d32f7d1100ff6d3cf008b60b4f66d2ae5b
mmichaelb <mmichaelb@protonmail.com>
2019-05-23 20:28:09 +0200

    Update session concept and add general session service

--- sessions.go
+++ sessions.go
@@ -2,56 +2,25 @@ package gosharexserver
 
 import (
 	"errors"
-	"time"
-
-	"github.com/google/uuid"
+	"net/http"
 )
 
 var (
 	// ErrSessionInvalid indicates that the given session has been invalidated.
 	ErrSessionInvalid = errors.New("the given session is invalid")
+	// ErrNoSessionSet indicates that the given request does not contain a session.
+	ErrNoSessionSet = errors.New("the given request does not contain a session")
 )
 
-// SessionService contains the basic functions to manage sessions and serialize/deserialize them.
+// SessionService contains the basic functions to manage sessions.
 type SessionService interface {
-	// NewUserSession returns a new session and saves it to the database. It returns an error if
+	// SetUserSession sets the session and saves it to the database. It returns an error if
 	// something went wrong.
-	NewUserSession(user *User, expiresOn time.Time) (session *UserSession, err error)
+	SetUserSession(user *User, resp http.ResponseWriter) (err error)
 	// InvalidateUserSessions invalidates all used user sessions and therefore automatically logs
 	// the user out of his account.
 	InvalidateUserSessions(user *User) (err error)
-	// DeserializeSession deserializes the user session by using the given raw session string. If
-	// the returned error is an ErrSessionInvalid, the session will still be parsed but it is not
-	// valid anymore. It returns an error (err) if something went wrong.
-	DeserializeSession(rawSession string) (session *UserService, err error)
-	// SerializeSession serializes the session and returns the raw serialized session as a string. It
-	// returns an error (err) if something went wrong.
-	SerializeSession(session *UserSession) (rawSession string, err error)
-}
-
-// UserSession contains the basic information needed within a session object. It does not contain any
-// serialization functions - these belong to the SessionService implementation.
-type UserSession struct {
-	// ExpiresOn holds the date when the session should expire.
-	ExpiresOn time.Time
-	// UserId holds the user`s id inside the database.
-	UserID uuid.UUID
-	// Username holds the user`s username.
-	Username string
-	// Role holds the user`s role.
-	Role Role
-}
-
-// GetUserInstance returns a new instantiated instance of the User type by using the given data.
-func (session UserSession) GetUserInstance() (user *User) {
-	return &User{
-		Id:       session.UserID,
-		Username: session.Username,
-		Role:     session.Role,
-	}
-}
-
-// IsActive returns whether the session is still active.
-func (session UserSession) IsActive() (active bool) {
-	return time.Now().Before(session.ExpiresOn)
+	// ValidateUserSession validates the http request and checks whether a session is present. If so,
+	// the matched user is returned.
+	ValidateUserSession(req *http.Request) (user *User, err error)
 }
