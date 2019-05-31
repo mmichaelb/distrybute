0486f1db2dcb711abab1c71071e548eb732801b4
mmichaelb <mmichaelb@protonmail.com>
2019-05-31 23:57:42 +0200

    Separate file managing and storing process

--- storage.go
+++ storage.go
@@ -1,6 +1,7 @@
 package gosharexserver
 
 import (
+	"errors"
 	"io"
 	"time"
 
@@ -14,9 +15,10 @@ const (
 	ContentTypeStatsMinimum = 10
 )
 
-// Storage holds all functions needed for a useable Storage implementation. It contains statistic
-// functions and the upload and deletion of file entries.
-type Storage interface {
+// FileManager holds all functions needed for a useable file manager implementation. It contains
+// statistic functions and the upload and deletion of file entries. It may uses a storage helper
+// implementation in order to separate the storage and metadata saving process.
+type FileManager interface {
 	// Store saves the entry data to the storage and returns the writer in order to write the file
 	// to the storage. If something went wrong, an error (err) is returned.
 	// In addition to that the entry`s ID and callReference fields are manipulated after the
@@ -28,7 +30,7 @@ type Storage interface {
 	// Delete tries to search for the entries by using any of the following values set in the entry
 	// instance: CallReference or DeleteReference. The entries are deleted if the search was
 	// successful. It returns an error (err) if something went wrong and the total number of deleted
-	// file entries).
+	// file entries.
 	Delete(entries []*FileEntry) (err error, deleted int64)
 	// ListEntries lists up all matched entries by searching for all entries by the given uuids and
 	// returns the matched ones. It also accepts a various number of parameters to modify the search
@@ -50,6 +52,32 @@ type Storage interface {
 	// according to ISO 8601 in a simplified version which is YYYYMMDDThhmmssZ and UTC.
 	// TODO: use the int64 unix time as a key.
 	UploadStatistics(uid []uuid.UUID, period time.Duration) (err error, stats *MapBasedStatistics)
+}
+
+var (
+	// ErrDuplicateStorageID indicates that the provided id is already present in the file storage.
+	ErrDuplicateStorageID = errors.New("the given id is already present in the file storage")
+	// ErrIDNotFound indicates that there is no such file with this id in the file storage.
+	ErrIDNotFound = errors.New("the given id was not found in the file storage")
+)
+
+// Storage holds functions which are only being called by a file manager and this interface is used
+// as a helper to separate the storing and managing process of files.
+type Storage interface {
+	// PutFile stores input in the given file storage. It returns a io.WriteCloser in order to
+	// allow a streamable saving process. In addition to that, it needs an identical string which
+	// is used to identify the object in further retrievals. If the id is already present in the
+	// file storage, ErrDuplicateStorageID is returned - if different errors occur, they are returned
+	// without wrapping.
+	PutFile(id string) (*io.WriteCloser, error)
+	// GetFile first searches for the given id and if found, returns the content by handing over a
+	// ReadCloseSeeker. If the given id cannot be found in the file entry, ErrIDNotFound is
+	// returned - if different errors occur, they are returned without wrapping.
+	GetFile(id string) (*ReadCloseSeeker, error)
+	// DeleteFile tries to delete the file associated with the given id. If the given id cannot be
+	// found in the file entry, ErrIDNotFound is returned - if different errors occur, they are
+	// returned without wrapping.
+	DeleteFile(id string) error
 }
 
 // MapBasedStatistics contains the key-based statistics needed for the statistics endpoint.
