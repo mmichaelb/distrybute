272011459548e37eae2dd1f070c42ec58381c7fb
mmichaelb <mmichaelb@protonmail.com>
2019-09-12 00:35:50 +0200

    Update PostgreSQL config and go module

--- go.mod
+++ go.mod
@@ -3,13 +3,20 @@ module github.com/mmichaelb/gosharexserver
 go 1.12
 
 require (
-	github.com/go-ini/ini v1.44.0 // indirect
+	github.com/go-ini/ini v1.46.0 // indirect
 	github.com/google/uuid v1.1.1
+	github.com/gopherjs/gopherjs v0.0.0-20190910122728-9d188e94fb99 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lib/pq v1.2.0
 	github.com/minio/minio-go v6.0.14+incompatible
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/smartystreets/assertions v1.0.1 // indirect
 	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337 // indirect
-	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
-	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
-	gopkg.in/ini.v1 v1.44.0 // indirect
+	golang.org/x/crypto v0.0.0-20190911031432-227b76d455e7 // indirect
+	golang.org/x/net v0.0.0-20190909003024-a7b16738d86b // indirect
+	golang.org/x/sys v0.0.0-20190911201528-7ad0cfa0b7b5 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/ini.v1 v1.46.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2
 )
--- postgres/config.go
+++ postgres/config.go
@@ -2,7 +2,10 @@ package postgres
 
 import (
 	"crypto/rand"
+	"gopkg.in/yaml.v2"
+	"io"
 	"math/big"
+	"os"
 	"strings"
 )
 
@@ -12,8 +15,32 @@ type Config struct {
 		EntryIDLength   int    `yaml:"entry_id_length"`
 		RemovalIDLength int    `yaml:"removal_id_length"`
 		Chars           string `yaml:"chars"`
-		charsRunes []rune
+		charsRunes      []rune
 	} `yaml:"entry_ids"`
+}
+
+// ReadConfigurationFile reads from the given filepath and if successful returns the parsed configuration.
+func ReadConfigurationFile(filePath string) (*Config, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := file.Close(); err !=nil {
+			panic(err) // if this err is non-nil panic is the only way out
+		}
+	}()
+	// forward the read process to the ReadConfiguration function
+	return ReadConfiguration(file)
+}
+
+// ReadConfiguration reads from the given file and if successful returns the parsed configuration.
+func ReadConfiguration(reader io.Reader) (*Config, error) {
+	decoder := yaml.NewDecoder(reader)
+	var config *Config
+	// use yaml decoder to decode the configuration file
+	err := decoder.Decode(config)
+	return config, err
 }
 
 // GenerateEntryID generates a new entry ID based on the values read from the configuration struct.
@@ -28,7 +55,7 @@ func (config Config) GenerateRemovalID() (string, error) {
 
 // generateUnfixedLengthID is an internal function in order to generate IDs based on the configuration values and using
 // the secure crypto/rand package.
-func (config Config) generateUnfixedLengthID(length int ) (string, error) {
+func (config Config) generateUnfixedLengthID(length int) (string, error) {
 	var stringBuilder strings.Builder
 	int64CharsRunesLength := int64(len(config.IDs.charsRunes))
 	for i := 0; i < length; i++ {
