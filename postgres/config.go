453ea09a1e00e5e42fa787dbb5a4dcbdd9e3b104
mmichaelb <mmichaelb@protonmail.com>
2019-09-18 14:55:29 +0200

    Add config to FileService and add Config Init function

--- postgres/config.go
+++ postgres/config.go
@@ -2,45 +2,23 @@ package postgres
 
 import (
 	"crypto/rand"
-	"gopkg.in/yaml.v2"
-	"io"
 	"math/big"
-	"os"
 	"strings"
 )
 
 // Config contains all values needed for a working PostgreSQL storage service.
 type Config struct {
 	IDs struct {
-		EntryIDLength   int    `yaml:"entry_id_length"`
-		RemovalIDLength int    `yaml:"removal_id_length"`
-		Chars           string `yaml:"chars"`
+		EntryIDLength   int
+		RemovalIDLength int
+		Chars           string
 		charsRunes      []rune
-	} `yaml:"entry_ids"`
-}
-
-// ReadConfigurationFile reads from the given filepath and if successful returns the parsed configuration.
-func ReadConfigurationFile(filePath string) (*Config, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
 	}
-	defer func() {
-		if err := file.Close(); err !=nil {
-			panic(err) // if this err is non-nil panic is the only way out
-		}
-	}()
-	// forward the read process to the ReadConfiguration function
-	return ReadConfiguration(file)
 }
 
-// ReadConfiguration reads from the given file and if successful returns the parsed configuration.
-func ReadConfiguration(reader io.Reader) (*Config, error) {
-	decoder := yaml.NewDecoder(reader)
-	var config *Config
-	// use yaml decoder to decode the configuration file
-	err := decoder.Decode(config)
-	return config, err
+// Init must be called before interacting with the Config.
+func (config *Config) Init() {
+	config.IDs.charsRunes = []rune(config.IDs.Chars)
 }
 
 // GenerateEntryID generates a new entry ID based on the values read from the configuration struct.
@@ -56,6 +34,9 @@ func (config Config) GenerateRemovalID() (string, error) {
 // generateUnfixedLengthID is an internal function in order to generate IDs based on the configuration values and using
 // the secure crypto/rand package.
 func (config Config) generateUnfixedLengthID(length int) (string, error) {
+	if config.IDs.charsRunes == nil {
+		panic("config Init function has to be called first")
+	}
 	var stringBuilder strings.Builder
 	int64CharsRunesLength := int64(len(config.IDs.charsRunes))
 	for i := 0; i < length; i++ {
--- postgres/instance.go
+++ postgres/instance.go
@@ -17,21 +17,24 @@ type Service struct {
 	gosharexserver.SessionService
 	db      *sql.DB
 	storage gosharexserver.Storage
+	config *Config
 }
 
 // New instantiates a new instance of the Postgres based Service.
-func New(db *sql.DB, storage gosharexserver.Storage) *Service {
+func New(db *sql.DB, storage gosharexserver.Storage, config *Config) *Service {
 	return &Service{
 		db:      db,
 		storage: storage,
+		config:config,
 	}
 }
 
 // NewWithConnectionString instantiates a new instance by using the given data source name (aka
 // connect url).
-func NewWithConnectionString(connectionString string, storage gosharexserver.Storage) (service *Service, err error) {
+func NewWithConnectionString(connectionString string, storage gosharexserver.Storage, config *Config) (service *Service, err error) {
 	service = &Service{
 		storage:storage,
+		config:config,
 	}
 	service.db, err = sql.Open("postgresql", connectionString)
 	return
