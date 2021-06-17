17d385ac82c5294039ae64a31b2131f4c2d924b3
mmichaelb <mmichaelb@protonmail.com>
2021-06-17 18:49:45 +0200

    add hash and tests for hash functions

---
+++ internal/postgres/hash.go
@@ -0,0 +1,42 @@
+package postgres
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
+	distrybute "github.com/mmichaelb/distrybute/internal"
+	"golang.org/x/crypto/argon2"
+)
+
+const (
+	saltLength      = 16
+	authTokenLength = 16
+)
+
+func generatePasswordUserEntry(password []byte, algorithm distrybute.PasswordHashAlgorithm) (hashedPassword []byte, salt []byte, err error) {
+	salt = make([]byte, saltLength)
+	if _, err = rand.Read(salt); err != nil {
+		return
+	}
+	hashedPassword, err = generatePasswordHash(password, salt, algorithm)
+	return hashedPassword, salt, err
+}
+
+func generatePasswordHash(password []byte, salt []byte, algorithm distrybute.PasswordHashAlgorithm) (hashedPassword []byte, err error) {
+	switch algorithm {
+	case distrybute.PasswordHashArgon2ID:
+		hashedPassword = argon2.IDKey(password, salt, 1, 64*1024, 4, 32)
+	default:
+		return nil, errors.New("the provided hashing algorithm is unknown")
+	}
+	return
+}
+
+func generateAuthToken() (authToken string, err error) {
+	authTokenBytes := make([]byte, authTokenLength)
+	_, err = rand.Read(authTokenBytes)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(authTokenBytes), nil
+}
---
+++ internal/postgres/hash_test.go
@@ -0,0 +1,56 @@
+package postgres
+
+import (
+	"encoding/hex"
+	distrybute "github.com/mmichaelb/distrybute/internal"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_generateAuthToken(t *testing.T) {
+	t.Run("returns an encoded hex value", func(t *testing.T) {
+		gotAuthToken, err := generateAuthToken()
+		assert.NoError(t, err)
+		_, decodeErr := hex.DecodeString(gotAuthToken)
+		assert.NoError(t, decodeErr)
+	})
+	t.Run("returns a long enough hex value", func(t *testing.T) {
+		gotAuthToken, err := generateAuthToken()
+		assert.NoError(t, err)
+		tokenBytes, decodeErr := hex.DecodeString(gotAuthToken)
+		assert.NoError(t, decodeErr)
+		assert.Len(t, tokenBytes, 16)
+	})
+	t.Run("generates different auth tokens", func(t *testing.T) {
+		firstGotAuthToken, err := generateAuthToken()
+		assert.NoError(t, err)
+		secondGotAuthToken, err := generateAuthToken()
+		assert.NoError(t, err)
+		assert.False(t, firstGotAuthToken == secondGotAuthToken)
+	})
+}
+
+func Test_generatePasswordHash(t *testing.T) {
+	t.Run("argon2id hash being generated correctly", func(t *testing.T) {
+		gotPasswordHash, err := generatePasswordHash([]byte("Sommer2019"), []byte("somegoodsalt"), distrybute.PasswordHashArgon2ID)
+		assert.NoError(t, err)
+		expectedHash := []byte{0xf5, 0x5f, 0xae, 0xf, 0xbd, 0x24, 0x81, 0x8e, 0xe5, 0xb7, 0x14, 0x7e, 0xee, 0x98, 0xa6, 0x50, 0xc3, 0xbc, 0xd1, 0x3, 0x34, 0xcb, 0xc8, 0x2b, 0x29, 0x44, 0x9c, 0x64, 0x2d, 0x22, 0xa8, 0x9d}
+		assert.Equal(t, expectedHash, gotPasswordHash)
+	})
+}
+
+func Test_generatePasswordUserEntry(t *testing.T) {
+	t.Run("salt being generated is of correct length", func(t *testing.T) {
+		_, salt, err := generatePasswordUserEntry([]byte("Sommer2019"), distrybute.PasswordHashArgon2ID)
+		assert.NoError(t, err)
+		assert.Len(t, salt, 16)
+	})
+	t.Run("password hash is being generated correctly", func(t *testing.T) {
+		password := []byte("Sommer2019")
+		gotPasswordHash, salt, err := generatePasswordUserEntry(password, distrybute.PasswordHashArgon2ID)
+		assert.NoError(t, err)
+		expectedPasswordHash, err := generatePasswordHash(password, salt, distrybute.PasswordHashArgon2ID)
+		assert.NoError(t, err)
+		assert.Equal(t, expectedPasswordHash, gotPasswordHash)
+	})
+}
