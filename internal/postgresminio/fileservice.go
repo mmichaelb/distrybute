87732c544ede4fcb75788d45d06b70934ffb645c
mmichaelb <mmichaelb@protonmail.com>
2021-06-18 17:04:27 +0200

    add store entry and ddl method

--- internal/postgresminio/fileservice.go
+++ internal/postgresminio/fileservice.go
@@ -1,35 +1,118 @@
 package postgresminio
 
 import (
+	"context"
+	"crypto/rand"
+	"errors"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4"
+	"github.com/minio/minio-go"
 	distrybute "github.com/mmichaelb/distrybute/internal"
+	"github.com/rs/zerolog/log"
 	"io"
+	"math/big"
+	"strings"
+	"time"
 )
 
-func (s *service) Store(entry *distrybute.FileEntry, reader io.Reader) (err error) {
+var entryIdChars = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
+const (
+	callReferenceLength   = 4
+	deleteReferenceLength = 12
+)
+
+func (s *service) initFileServiceDDL() error {
+	row := s.connection.QueryRow(context.Background(), `CREATE TABLE IF NOT EXISTS distrybute.entries (
+		id uuid,
+		author uuid NOT NULL,
+		call_reference varchar(4) NOT NULL,
+		delete_reference varchar(12) NOT NULL ,
+		filename varchar(256) NOT NULL,
+		content_type varchar(127) NOT NULL,
+		upload_date timestamptz NOT NULL,
+		size bigint,
+		CONSTRAINT entries_pk PRIMARY KEY (id),
+		CONSTRAINT entries_fk FOREIGN KEY (author) REFERENCES distrybute.users(id),
+		CONSTRAINT entries_call_reference_unique UNIQUE (call_reference),
+		CONSTRAINT entries_delete_reference_unique UNIQUE (delete_reference)
+	);`)
+	if err := row.Scan(); !errors.Is(err, pgx.ErrNoRows) {
+		log.Err(err).Msg("could not run initial file service ddl")
+		return err
+	}
+	return nil
 }
 
-func (s *service) Request(callReference string) (entry distrybute.FileEntry, err error) {
+func (s *service) Store(filename, contentType string, size int64, author uuid.UUID, reader io.Reader) (entry *distrybute.FileEntry, err error) {
+	tx, err := s.connection.Begin(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	defer func(tx pgx.Tx, ctx context.Context) {
+		err := tx.Rollback(ctx)
+		if !errors.Is(err, pgx.ErrTxClosed) {
+			log.Err(err).Str("filename", filename).Str("contentType", contentType).Msg("could not close transaction opened in order to store a new entry")
+		}
+	}(tx, context.Background())
+	id, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+	callReference, err := generateEntryId(callReferenceLength)
+	if err != nil {
+		return nil, err
+	}
+	deleteReference, err := generateEntryId(deleteReferenceLength)
+	if err != nil {
+		return nil, err
+	}
+	uploadDate := time.Now()
+	row := tx.QueryRow(context.Background(),
+		`INSERT INTO distrybute.entries (id, author, call_reference, delete_reference, filename, content_type, upload_date, size)
+ VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`, id, author, callReference, deleteReference, filename, contentType, uploadDate, size)
+	if err := row.Scan(); !errors.Is(err, pgx.ErrNoRows) {
+		return nil, err
+	}
+	_, err = s.minioClient.PutObject(s.bucketName, s.objectPrefix+id.String(), reader, size, minio.PutObjectOptions{})
+	if err != nil {
+		return nil, err
+	}
+	err = tx.Commit(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	entry = &distrybute.FileEntry{
+		Id:              id,
+		CallReference:   callReference,
+		DeleteReference: deleteReference,
+		Author:          author,
+		Filename:        filename,
+		ContentType:     contentType,
+		UploadDate:      uploadDate,
+		Size:            size,
+	}
+	return entry, nil
+}
+
+func (s *service) Request(callReference string) (entry *distrybute.FileEntry, err error) {
 	panic("implement me")
 }
 
-func (s *service) Delete(entries chan string) (responseChan chan distrybute.DeleteResponse) {
+func (s *service) Delete(deleteReference string) (err error) {
 	panic("implement me")
 }
 
-func (s *service) ListEntries(limit int, offset int, sortBy distrybute.FileEntrySortElem, sortOrder distrybute.SortSequence, uuids ...uuid.UUID) (entries []distrybute.FileEntry, err error) {
-	panic("implement me")
-}
-
-func (s *service) SearchEntries(query string, limit int, offset int, sortBy distrybute.FileEntrySortElem, sortOrder distrybute.SortSequence, uuids ...uuid.UUID) (entries []distrybute.FileEntry, err error) {
-	panic("implement me")
-}
-
-func (s *service) ResolveMIMETypeStatistic(uuids ...uuid.UUID) (totalEntries int64, statistic distrybute.MIMETypeStatistic, err error) {
-	panic("implement me")
-}
-
-func (s *service) ResolveUserUploadPeriodStatistic(uuid uuid.UUID, period distrybute.Period) (statistic distrybute.UserUploadPeriodStatistic, err error) {
-	panic("implement me")
+func generateEntryId(length int) (string, error) {
+	var id strings.Builder
+	for i := 0; i < length; i++ {
+		randIndex, err := rand.Int(rand.Reader, big.NewInt(int64(len(entryIdChars))))
+		if err != nil {
+			return "", err
+		}
+		if _, err = id.WriteRune(entryIdChars[randIndex.Int64()]); err != nil {
+			return "", err
+		}
+	}
+	return id.String(), nil
 }
