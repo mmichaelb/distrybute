69dcfe237ea769d713e97f49bb490b540d098609
mmichaelb <mmichaelb@protonmail.com>
2021-06-18 17:11:03 +0200

    add minio variables to postgres minio service type

--- internal/postgresminio/service.go
+++ internal/postgresminio/service.go
@@ -2,10 +2,14 @@ package postgresminio
 
 import (
 	"github.com/jackc/pgx/v4"
+	"github.com/minio/minio-go"
 )
 
 type service struct {
-	connection *pgx.Conn
+	connection   *pgx.Conn
+	minioClient  *minio.Client
+	bucketName   string
+	objectPrefix string
 }
 
 func NewService(connection *pgx.Conn) *service {
