8ec7427b3c7fe998f1e986332698c11844f5254c
mmichaelb <mmichaelb@protonmail.com>
2021-06-18 17:02:13 +0200

    remove active field from entry and update file service store method

--- internal/file.go
+++ internal/file.go
@@ -2,6 +2,7 @@ package distrybute
 
 import (
 	"errors"
+	"github.com/google/uuid"
 	"io"
 )
 
@@ -13,7 +14,7 @@ var (
 // FileService holds all functions needed for a usable file service implementation.
 type FileService interface {
 	// Store saves the entry data to the storage. If something went wrong, an error is returned.
-	Store(filename, contentType string, size int64, reader io.Reader) (entry *FileEntry, err error)
+	Store(filename, contentType string, size int64, author uuid.UUID, reader io.Reader) (entry *FileEntry, err error)
 	// Request searches for an entry by using the specified CallReference. It returns an error if something goes wrong.
 	Request(callReference string) (entry *FileEntry, err error)
 	// Delete deletes an entry using the provided delete reference. It returns an error if something goes wrong.
