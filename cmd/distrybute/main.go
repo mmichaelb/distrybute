388984f161c38294824fc8520a94f80ca15bb253
mmichaelb <mmichaelb@protonmail.com>
2021-06-18 18:22:17 +0200

    update main go file

--- cmd/distrybute/main.go
+++ cmd/distrybute/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/mmichaelb/distrybute/internal/web/rest"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
+	logFile, err := os.Create("log.json")
+	if err != nil {
+		panic(err)
+	}
+	log.Logger = log.Output(io.MultiWriter(zerolog.ConsoleWriter{
+		Out: os.Stdout,
+	}, logFile))
 	router := chi.NewRouter()
-	apiRouter := rest.NewRouter(log.With().Str("rest").Logger(), nil, nil)
-	router.Mount("/api/")
+	//	apiRouter := rest.NewRouter(log.With().Str("service", "rest").Logger(), nil, nil)
+	//	router.Mount("/api/", apiRouter.BuildHttpHandler())
+	panic(http.ListenAndServe("127.0.0.1:8080", router))
 }
