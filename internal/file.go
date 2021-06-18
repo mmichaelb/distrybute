0cd3ea0900ba6b3d07082acbb9c5cbd6f947f261
mmichaelb <mmichaelb@protonmail.com>
2021-06-18 16:22:12 +0200

    update comments for file service code

--- internal/file.go
+++ internal/file.go
@@ -10,20 +10,12 @@ var (
 	ErrEntryNotFound = errors.New("the given entry was not found in the file storage")
 )
 
-// FileService holds all functions needed for a usable file service implementation. It contains
-// statistic functions and the upload and deletion of file entries. It may uses a storage helper
-// implementation in order to separate the storage and metadata saving process.
+// FileService holds all functions needed for a usable file service implementation.
 type FileService interface {
-	// Store saves the entry data to the storage and returns the writer in order to write the file
-	// to the storage. If something went wrong, an error (err) is returned.
-	// In addition to that the entry`s ID, CallReference and (maybe) DeleteReference fields are manipulated after the
-	// function has finished.
+	// Store saves the entry data to the storage. If something went wrong, an error is returned.
 	Store(filename, contentType string, size int64, reader io.Reader) (entry *FileEntry, err error)
-	// Request searches for an entry by using the specified CallReference. It returns an error if
-	// something went wrong.
+	// Request searches for an entry by using the specified CallReference. It returns an error if something goes wrong.
 	Request(callReference string) (entry *FileEntry, err error)
-	// Delete deletes all entries by using the specified entry ids. It returns a responseChan so that the
-	// requesting party can keep track of failed/successful deletions.
-	// You should read permanently from the channel because it otherwise will block the deletion process.
+	// Delete deletes an entry using the provided delete reference. It returns an error if something goes wrong.
 	Delete(deleteReference string) (err error)
 }
