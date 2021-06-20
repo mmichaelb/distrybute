360ad84a44808730aa56b405d08300e7116c3483
mmichaelb <mmichaelb@protonmail.com>
2021-06-20 17:54:57 +0200

    expose postgres port and pass connect env vars

--- internal/postgresminio/user_test.go
+++ internal/postgresminio/user_test.go
@@ -2,13 +2,17 @@ package postgresminio
 
 import (
 	"context"
+	"fmt"
 	"github.com/jackc/pgx/v4"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 func Test_PostgresMinio_UserService(t *testing.T) {
-	connection, err := pgx.Connect(context.Background(), "postgres://postgres:postgres@localhost:5432/postgres")
+	host := os.Getenv("TEST_POSTGRES_HOST")
+	port := os.Getenv("TEST_POSTGRES_PORT")
+	connection, err := pgx.Connect(context.Background(), fmt.Sprintf("postgres://postgres:postgres@%s:%s/postgres", host, port))
 	defer connection.Close(context.Background())
 	assert.NoError(t, err)
 }
