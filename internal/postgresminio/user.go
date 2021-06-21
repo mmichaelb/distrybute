00b33aca3da8bdedb0b20548f817a858b388f0f5
mmichaelb <mmichaelb@protonmail.com>
2021-06-21 12:22:51 +0200

    fix sql error in user ddl

--- internal/postgresminio/user.go
+++ internal/postgresminio/user.go
@@ -20,7 +20,7 @@ func (s *service) initUserDDL() (err error) {
 		password_salt bytea NOT NULL,
 		"password" bytea NOT NULL,
 		CONSTRAINT users_pk PRIMARY KEY (id),
-		CONSTRAINT users_auth_token_unique UNIQUE (auth_token),
+		CONSTRAINT users_auth_token_unique UNIQUE (auth_token)
 	)`).Scan()
 	if !errors.Is(err, pgx.ErrNoRows) {
 		return fmt.Errorf("error occurred while running user table creation ddl: %w", err)
