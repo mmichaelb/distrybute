d836865f3e3058e35ac70d40d57f155ef9553914
mmichaelb <mmichaelb@protonmail.com>
2021-06-23 00:13:04 +0200

    fix return logic in validate user session method

--- internal/postgresminio/sessions.go
+++ internal/postgresminio/sessions.go
@@ -80,7 +80,9 @@ func (s *service) InvalidateUserSessions(user *distrybute.User) (err error) {
 
 func (s *service) ValidateUserSession(req *http.Request) (bool, *http.Request, error) {
 	cookie, err := req.Cookie(sessionCookieName)
-	if err != nil && !errors.Is(err, http.ErrNoCookie) {
+	if errors.Is(err, http.ErrNoCookie) {
+		return false, req, nil
+	} else if err != nil {
 		return false, req, err
 	}
 	sessionKey := cookie.Value
