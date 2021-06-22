0fa58cc7e873660da418c0e629ada2fdac07e9a0
mmichaelb <mmichaelb@protonmail.com>
2021-06-23 00:45:13 +0200

    add example main file

--- cmd/distrybute/main.go
+++ cmd/distrybute/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v4"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/mmichaelb/distrybute/internal/postgresminio"
+	"github.com/mmichaelb/distrybute/internal/web/rest"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"io"
@@ -18,7 +24,22 @@ func main() {
 		Out: os.Stdout,
 	}, logFile))
 	router := chi.NewRouter()
-	//	apiRouter := rest.NewRouter(log.With().Str("service", "rest").Logger(), nil, nil)
-	//	router.Mount("/api/", apiRouter.BuildHttpHandler())
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres:postgres@localhost:5432/postgres")
+	if err != nil {
+		panic(err)
+	}
+	minioClient, err := minio.New("localhost:9000", &minio.Options{
+		Creds: credentials.NewStaticV4("minioadmin", "minioadmin", ""),
+	})
+	if err != nil {
+		panic(err)
+	}
+	service := postgresminio.NewService(conn, minioClient, "distrybute", "file-")
+	if err = service.InitDDL(); err != nil {
+		panic(err)
+	}
+	apiRouter := rest.NewRouter(log.With().Str("service", "rest").Logger(),
+		service, service, service, []byte("somerandombytes"))
+	router.Mount("/api/", apiRouter.BuildHttpHandler())
 	panic(http.ListenAndServe("127.0.0.1:8080", router))
 }
