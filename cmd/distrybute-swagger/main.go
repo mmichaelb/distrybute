f0bb6981c949be9eec3b0236d1df9edbccb769e2
mmichaelb <mmichaelb@protonmail.com>
2021-10-29 21:28:31 +0200

    adjust swagger.json path in swagger web server file

--- cmd/distrybute-swagger/main.go
+++ cmd/distrybute-swagger/main.go
@@ -15,7 +15,7 @@ func main() {
 	port := flag.Int("port", 33761, "the port the web server should listen on")
 	router := chi.NewRouter()
 	router.Handle("/*", httpSwagger.Handler(
-		httpSwagger.URL("http://localhost:33761/swagger/doc.json"),
+		httpSwagger.URL("/swagger/doc.json"),
 	))
 	addr := net.JoinHostPort(*host, strconv.Itoa(*port))
 	if err := http.ListenAndServe(addr, router); err != nil && err != http.ErrServerClosed {
