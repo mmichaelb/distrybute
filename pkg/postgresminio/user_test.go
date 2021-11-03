085de2848dcb83d5163cebe6413eac59c0d230cb
mmichaelb <mmichaelb@protonmail.com>
2021-11-03 15:25:09 +0100

    remove user service integration test redundancies

--- pkg/postgresminio/user_test.go
+++ pkg/postgresminio/user_test.go
@@ -10,38 +10,30 @@ import (
 
 func userServiceIntegrationTest(userService distrybute.UserService) func(t *testing.T) {
 	return func(t *testing.T) {
+		const usernameBasicTest = "usertest-user"
+		var userBasicTestId uuid.UUID
 		t.Run("user is created correctly", func(t *testing.T) {
-			const username = "usertest-user"
-			user, err := userService.CreateNewUser(username, []byte("somepassword"))
+			user, err := userService.CreateNewUser(usernameBasicTest, []byte("somepassword"))
 			assert.NoError(t, err)
 			assert.NotEqual(t, uuid.UUID{}, user.ID)
-			assert.Equal(t, username, user.Username)
+			userBasicTestId = user.ID
+			assert.Equal(t, usernameBasicTest, user.Username)
 			assert.NotEmpty(t, user.AuthorizationToken)
 			assert.Equal(t, distrybute.LatestPasswordHashAlgorithm, user.PasswordHashAlgorithm)
 		})
 		t.Run("duplicate usernames are not accepted", func(t *testing.T) {
-			const username = "usertest-duplicate"
-			_, err := userService.CreateNewUser(username, []byte("Sommer2019"))
-			assert.NoError(t, err)
-			_, err = userService.CreateNewUser(username, []byte("Sommer2020"))
+			_, err := userService.CreateNewUser(usernameBasicTest, []byte("Sommer2020"))
 			assert.ErrorIs(t, err, distrybute.ErrUserAlreadyExists)
 		})
 		t.Run("duplicate usernames are being detected case insensitively", func(t *testing.T) {
-			const username = "usertest-duplicate-case-ins"
-			const usernameSecond = "usertest-duplicate-case-INS"
-			_, err := userService.CreateNewUser(username, []byte("Sommer2019"))
-			assert.NoError(t, err)
-			_, err = userService.CreateNewUser(usernameSecond, []byte("Sommer2020"))
+			_, err := userService.CreateNewUser(strings.ToUpper(usernameBasicTest), []byte("Sommer2020"))
 			assert.ErrorIs(t, err, distrybute.ErrUserAlreadyExists)
 		})
 		t.Run("user deletion test", func(t *testing.T) {
 			t.Run("user is deleted correctly", func(t *testing.T) {
-				const username = "usertest-del"
-				user, err := userService.CreateNewUser(username, []byte("Testpassword"))
+				err := userService.DeleteUser(userBasicTestId)
 				assert.NoError(t, err)
-				err = userService.DeleteUser(user.ID)
-				assert.NoError(t, err)
-				_, _, err = userService.CheckPassword(username, []byte("Testpassword"))
+				_, _, err = userService.CheckPassword(usernameBasicTest, []byte("Testpassword"))
 				assert.ErrorIs(t, err, distrybute.ErrUserNotFound)
 			})
 			t.Run("user cannot be deleted if not present", func(t *testing.T) {
