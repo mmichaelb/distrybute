2ee3e38572fece040855501e43f31733eb3b4859
mmichaelb <mmichaelb@protonmail.com>
2021-11-24 01:19:35 +0100

    move middlewares to extra file

---
+++ pkg/rest/controller/middlewares.go
@@ -0,0 +1,58 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/rs/zerolog/hlog"
+	"net/http"
+)
+
+func (r *router) setupMiddlewares() {
+	middleware.RequestIDHeader = ""
+	r.Use(hlog.NewHandler(r.logger))
+	r.Use(middleware.CleanPath)
+	r.Use(hlog.RequestIDHandler("request_id", ""))
+	r.Use(r.loggingMiddleware)
+	r.Use(r.recovererMiddleware)
+	r.NotFound(func(writer http.ResponseWriter, request *http.Request) {
+		r.wrapResponseWriter(writer).WriteAutomaticErrorResponse(http.StatusNotFound, nil, request)
+	})
+	r.MethodNotAllowed(func(writer http.ResponseWriter, request *http.Request) {
+		r.wrapResponseWriter(writer).WriteAutomaticErrorResponse(http.StatusMethodNotAllowed, nil, request)
+	})
+}
+
+func (r *router) loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		hlog.FromRequest(request).Info().
+			Str("addr", request.RemoteAddr).
+			Str("userAgent", request.Header.Get("User-Agent")).
+			Str("method", request.Method).
+			Str("path", request.RequestURI).
+			Send()
+		wrappedWriter := r.wrapResponseWriter(writer)
+		defer func() {
+			hlog.FromRequest(request).Info().
+				Int("responseCode", wrappedWriter.statusCode).Send()
+		}()
+		next.ServeHTTP(wrappedWriter, request)
+	})
+}
+
+func (r *router) recovererMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			recoveredValue := recover()
+			if recoveredValue == nil || recoveredValue == http.ErrAbortHandler {
+				return
+			}
+			if err, ok := recoveredValue.(error); ok {
+				hlog.FromRequest(request).Err(err).Msg("recovered an error from an http handler")
+			} else {
+				hlog.FromRequest(request).Error().Str("recoveredValue", fmt.Sprintf("%+v", recoveredValue)).
+					Msg("recovered an unknown value from an http handler")
+			}
+		}()
+		next.ServeHTTP(writer, request)
+	})
+}
