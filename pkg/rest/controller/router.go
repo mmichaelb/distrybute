9560b2b0fe7d47300828c54a80ff2e8da98e1288
mmichaelb <mmichaelb@protonmail.com>
2021-11-24 01:22:08 +0100

    remove token auth handler func wrap

--- pkg/rest/controller/router.go
+++ pkg/rest/controller/router.go
@@ -4,7 +4,6 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/mmichaelb/distrybute/pkg"
 	"github.com/rs/zerolog"
-	"net/http"
 )
 
 // @title distrybute API
@@ -13,8 +12,6 @@ import (
 
 // @license.name MIT
 // @license.url https://github.com/mmichaelb/distrybute/blob/master/LICENSE
-
-var userContextKey = &struct{}{}
 
 type router struct {
 	*chi.Mux
@@ -32,15 +29,4 @@ func NewRouter(logger zerolog.Logger, fileService distrybute.FileService, userSe
 	}
 	router.setupMiddlewares()
 	return router
-}
-
-func (r *router) handlerFuncWithAuth(handlerFn http.HandlerFunc) http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		user := request.Context().Value(userContextKey).(*distrybute.User)
-		if user == nil {
-			r.wrapResponseWriter(writer).WriteAutomaticErrorResponse(http.StatusUnauthorized, nil, request)
-			return
-		}
-		handlerFn.ServeHTTP(writer, request)
-	}
 }
